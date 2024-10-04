fc2d9c4774bca9ebe2e3598e8903a84fd8c98fae
James Dong <jamesjd1024@gmail.com>
2024-10-04 13:49:02 -0500

    project descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         </div>
       </section>
     );
-  };
+  };  
 
   const ProjectPreviews : React.FC = () => {
     return (
@@ -24,18 +24,18 @@ export default function Home() {
         <div className="mt-8 grid gap-8 md:grid-cols-3 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="bg-tan-500 p-6 rounded-lg shadow-lg">
             <Image src="/project1.jpg" alt="Project 1" width={500} height={300} className="rounded-md"/>
-            <h3 className="mt-4 text-xl font-semibold text-deepBlue-500">Project Name</h3>
-            <p className="mt-2 text-brown-500">Brief description of the project and the technologies used.</p>
+            <h3 className="mt-4 text-xl font-semibold text-deepBlue-500">Etsy Store Automation</h3>
+            <p className="mt-2 text-brown-500">Droppshipping has grown in popularity with the boom of ecommerce. This tool helps users easily create hundreds of listings automatically</p>
           </div>
           <div className="bg-tan-500 p-6 rounded-lg shadow-lg">
             <Image src="/project2.jpg" alt="Project 2" width={500} height={300} className="rounded-md"/>
-            <h3 className="mt-4 text-xl font-semibold text-deepBlue-500">Project Name</h3>
-            <p className="mt-2 text-brown-500">Brief description of the project and the technologies used.</p>
+            <h3 className="mt-4 text-xl font-semibold text-deepBlue-500">API Documentation Assistant</h3>
+            <p className="mt-2 text-brown-500">Proper software documentation can be a huge challenge. The Docket application helps users simplify the api documentation process </p>
           </div>
           <div className="bg-tan-500 p-6 rounded-lg shadow-lg">
             <Image src="/project3.jpg" alt="Project 3" width={500} height={300} className="rounded-md"/>
-            <h3 className="mt-4 text-xl font-semibold text-deepBlue-500">Project Name</h3>
-            <p className="mt-2 text-brown-500">Brief description of the project and the technologies used.</p>
+            <h3 className="mt-4 text-xl font-semibold text-deepBlue-500">Restaurant Food Waste Reduction</h3>
+            <p className="mt-2 text-brown-500">Starting as a part time job, James took a deep dive to using AI tools to help restaurants manage food waste</p>
           </div>
         </div>
       </section>
