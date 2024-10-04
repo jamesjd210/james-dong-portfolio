f52790feb4bf65ecf747f57e6ae134b0a8e2409b
James Dong <jamesjd1024@gmail.com>
2024-10-04 14:06:28 -0500

    footer outline

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "@/styles/globals.css"
 import Header from "@/components/Header"
+import Footer from "@/components/Footer"
 
 const geistSans = localFont({
   src: "../public/fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Header/>
         <main>{children}</main>
+        <Footer/>
       </body>
     </html>
   );
