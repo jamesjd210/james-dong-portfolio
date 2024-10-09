59b00b5706b48cdaa705d88e622a7744d701f924
James Dong <jamesjd1024@gmail.com>
2024-10-09 10:14:10 -0500

    footer complete

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,20 +7,20 @@ export default function Footer() {
       <div className="py-12 md:py-16">
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
           
-          {/* Top area: Blocks */ }
+          {/* Top area: Blocks
           <div className="grid md:grid-cols-12 gap-8 lg:gap-20 mb-8 md:mb-12">
 
-            {/* 1st block */}
+            {/* 1st block 
             <div className="md:col-span-4 lg:col-span-5">
               <div className="mb-2">
               </div>
               <div className="text-gray-400">Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</div>
             </div>
 
-            {/* 2nd, 3rd and 4th blocks */}
+            {/* 2nd, 3rd and 4th blocks
             <div className="md:col-span-8 lg:col-span-7 grid sm:grid-cols-3 gap-8">
 
-              {/* 2nd block */}
+              {/* 2nd block 
               <div className="text-sm">
                 <h6 className="text-gray-200 font-medium mb-1">Products</h6>
                 <ul>
@@ -36,7 +36,7 @@ export default function Footer() {
                 </ul>
               </div>
 
-              {/* 3rd block */}
+              {/* 3rd block
               <div className="text-sm">
                 <h6 className="text-gray-200 font-medium mb-1">Resources</h6>
                 <ul>
@@ -58,7 +58,7 @@ export default function Footer() {
                 </ul>
               </div>
 
-              {/* 4th block */}
+              {/* 4th block
               <div className="text-sm">
                 <h6 className="text-gray-200 font-medium mb-1">Company</h6>
                 <ul>
@@ -83,7 +83,7 @@ export default function Footer() {
             </div>
 
           </div>
-
+            */}
           {/* Bottom area */}
           <div className="md:flex md:items-center md:justify-between">
 
@@ -98,7 +98,7 @@ export default function Footer() {
                 </Link>
               </li> */}
               <li className="ml-4">
-                <Link href="https://github.com/jamesjd210" className="flex justify-center items-center text-blue-600 bg-gray-800 hover:text-gray-100 hover:bg-blue-600 rounded-full transition duration-150 ease-in-out" aria-label="Github" target = "_blank">
+                <Link href="https://github.com/jamesjd210" className="flex justify-center items-center text-deepBlue-500 bg-tan-500 hover:text-gray-100 hover:bg-biege-500 rounded-full transition duration-150 ease-in-out" aria-label="Github" target = "_blank">
                   <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                     <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
                   </svg>
@@ -123,7 +123,7 @@ export default function Footer() {
                 </Link>
               </li> */}
               <li className="ml-4">
-                <Link href="https://www.linkedin.com/in/james-dong-1024/ " className="flex justify-center items-center text-blue-600 bg-gray-800 hover:text-gray-100 hover:bg-blue-600 rounded-full transition duration-150 ease-in-out" aria-label="Linkedin" target="_blank">
+                <Link href="https://www.linkedin.com/in/james-dong-1024/ " className="flex justify-center items-center text-deepBlue-500 bg-tan-500 hover:text-gray-100 hover:bg-biege-500 rounded-full transition duration-150 ease-in-out" aria-label="Linkedin" target="_blank">
                   <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                     <path d="M23.3 8H8.7c-.4 0-.7.3-.7.7v14.7c0 .3.3.6.7.6h14.7c.4 0 .7-.3.7-.7V8.7c-.1-.4-.4-.7-.8-.7zM12.7 21.6h-2.3V14h2.4v7.6h-.1zM11.6 13c-.8 0-1.4-.7-1.4-1.4 0-.8.6-1.4 1.4-1.4.8 0 1.4.6 1.4 1.4-.1.7-.7 1.4-1.4 1.4zm10 8.6h-2.4v-3.7c0-.9 0-2-1.2-2s-1.4 1-1.4 2v3.8h-2.4V14h2.3v1c.3-.6 1.1-1.2 2.2-1.2 2.4 0 2.8 1.6 2.8 3.6v4.2h.1z" />
                   </svg>
@@ -132,7 +132,7 @@ export default function Footer() {
             </ul>
 
             {/* Copyrights note */}
-            <div className="text-gray-400 text-sm mr-4">&copy; jamesdong.dev. All rights reserved.</div>
+            <div className="text-gray-400 text-sm mr-4"> jamesdong.dev - James Dong's Personal Website</div>
 
           </div>
 
