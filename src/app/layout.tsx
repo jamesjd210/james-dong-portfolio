94b94920beeffb927c90dbed51b4451e39ea3c3b
James Dong <jamesjd1024@gmail.com>
2024-10-22 22:44:57 -0500

    removed local font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "@/styles/globals.css"
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-
-// const geistSans = localFont({
-//   src: "/public/static/fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "../public/static/fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 export const metadata: Metadata = {
   title: "James Dong",
@@ -27,7 +15,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}> */}
       <body>
         <Header/>
         <main>{children}</main>
