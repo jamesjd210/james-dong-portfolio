c32fae0e214ca9b1a5ab23f89921db2c69cc1d35
James Dong <jamesjd1024@gmail.com>
2024-11-11 23:44:06 -0600

    fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import React from "react";
 import { Project } from "@/types/Project"
 import { loadProjects } from "@/utils/loadProjects"; // function to load projects
