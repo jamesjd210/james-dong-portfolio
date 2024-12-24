196ef198841123d7c1e9ac3570397f84d3f86fa2
James Dong <jamesjd1024@gmail.com>
2024-12-23 22:39:37 -0600

    start work on contact

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,5 +1,12 @@
 export default function Portfolio() {
     return (
-        <div>Portfolio page</div>
+        <section className="bg-background py-16 px-8 lg:px-24 text-foreground">
+        {/* Hero Section */}
+            <div className="mx-auto text-center max-w-5xl">
+                <h1 className="text-4xl font-bold mb-6 lg:text-5xl">
+                    Check Out my Projects
+                </h1>
+            </div>
+        </section>
     );
 }
